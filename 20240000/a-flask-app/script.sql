f72190efe9ac4c5df960cfe66a10fce920a3a979
IDT02Jrada <155570404+IDT02Jrada@users.noreply.github.com>
2024-06-29 15:24:34 +0000

    edit inutile

--- 20240000/a-flask-app/script.sql
+++ 20240000/a-flask-app/script.sql
@@ -5,4 +5,6 @@ CREATE TABLE employees (
   last_name varchar(45) DEFAULT NULL
 );
 
-INSERT INTO employees (first_name, last_name) VALUES ('eugenio', 'lezzio');
+INSERT INTO employees (first_name, last_name) VALUES ('Eugenio', 'Lezzio');
+INSERT INTO employees (first_name, last_name) VALUES ('Marcolino', 'Reboani');
+INSERT INTO employees (first_name, last_name) VALUES ('Giuseppe', 'Grandi');
